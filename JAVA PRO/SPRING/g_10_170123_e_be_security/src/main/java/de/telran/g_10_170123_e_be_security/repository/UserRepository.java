19cc09b2ca180f6475dec9332835ea7208a65b13
LeoGordin <lev.grdn@gmail.com>
2023-10-25 14:58:23 +0300

    initial commit

---
+++ JAVA PRO/SPRING/g_10_170123_e_be_security/src/main/java/de/telran/g_10_170123_e_be_security/repository/UserRepository.java
@@ -0,0 +1,2 @@
+package de.telran.g_10_170123_e_be_security.repository;public interface UserRepository {
+}
---
+++ PROJECTS/Bank_proj/src/main/java/BankProject/TelRanBankProjApplication.java
@@ -0,0 +1,13 @@
+package BankProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelRanBankProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelRanBankProjApplication.class, args);
+	}
+
+}
